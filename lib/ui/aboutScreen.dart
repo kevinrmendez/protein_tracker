2ba22e490db628b036a51191ea3cc02e9a023d32
Kevin Mendez <kevinrmendez@gmail.com>
2020-07-17 14:54:11 +0300

    about sceen changed

--- lib/ui/aboutScreen.dart
+++ lib/ui/aboutScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:protein_tracker/main.dart';
+import 'package:protein_tracker/utils/colors.dart';
 import 'package:protein_tracker/utils/widgetUtils.dart';
 
 class AboutScreen extends StatelessWidget {
@@ -10,42 +11,46 @@ class AboutScreen extends StatelessWidget {
     return Scaffold(
       appBar: WidgetUtils.appBarBackArrow('About protein tracker', context),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 40),
+        // padding: EdgeInsets.symmetric(vertical: 40),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            WidgetUtils.card(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Protein Tracker',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                        // color: DarkGreyColor,
+                        ),
+                  ),
+                ),
+                color: PrimaryColor),
             Container(
-              width: MediaQuery.of(context).size.width * .7,
-              margin: EdgeInsets.only(bottom: 20),
-              child: _title(context),
+              margin: EdgeInsets.only(top: 60),
+              child: Column(
+                children: <Widget>[
+                  _text(
+                      'Protein tracker is a free app that helps you to calculate and track your protein intake'),
+                  _text(
+                      "In order to keep free the app for everybody and support it's development, it contains some ads"),
+                  _text('version 1.0.0'),
+                ],
+              ),
             ),
-            _text(
-                'protein tracker is a free app that helps you to track your protein intake'),
-            _text('version 1.0.0'),
           ],
         ),
       ),
     );
   }
 
-  Widget _title(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          'About',
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.display1,
-        ),
-        Text(
-          'protein tracker',
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.display1,
-        ),
-      ],
-    );
-  }
-
   Widget _text(text) => Container(
-      margin: EdgeInsets.symmetric(vertical: 20),
+      margin: EdgeInsets.symmetric(vertical: 10),
       padding: EdgeInsets.symmetric(horizontal: 20),
       child: Text(
         text,
