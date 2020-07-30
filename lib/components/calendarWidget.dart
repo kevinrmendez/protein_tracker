b95d4cce2ef8cc5061a205ab11696ae0d5507373
Kevin Mendez <kevinrmendez@gmail.com>
2020-07-30 16:14:16 +0300

    add calendar labels

--- lib/components/calendarWidget.dart
+++ lib/components/calendarWidget.dart
@@ -149,7 +149,7 @@ class _CalendarWidgetState extends State<CalendarWidget> {
       // firstDayOfWeek: 4,
       markedDatesMap: _markedDateMap,
       // height: 420.0,
-      height: MediaQuery.of(context).size.height * .45,
+      height: MediaQuery.of(context).size.height * .4,
       selectedDateTime: _currentDate,
       targetDateTime: _targetDateTime,
       customGridViewPhysics: NeverScrollableScrollPhysics(),
@@ -246,7 +246,7 @@ class _CalendarWidgetState extends State<CalendarWidget> {
           ),
         ),
         Container(
-          height: MediaQuery.of(context).size.height * .45,
+          height: MediaQuery.of(context).size.height * .4,
           margin: EdgeInsets.symmetric(horizontal: 16.0),
           child: _calendarCarouselNoHeader,
         ), //
--- lib/ui/calendarScreen.dart
+++ lib/ui/calendarScreen.dart
@@ -4,10 +4,38 @@ import 'package:protein_tracker/bloc/DailyProteinService.dart';
 import 'package:protein_tracker/components/calendarWidget.dart';
 import 'package:protein_tracker/model/dailyProtein.dart';
 import 'package:protein_tracker/utils/AdMobUtils.dart';
+import 'package:protein_tracker/utils/colors.dart';
 import 'package:protein_tracker/utils/localization_utils.dart';
 import 'package:protein_tracker/utils/widgetUtils.dart';
 
 class CalendarScreen extends StatelessWidget {
+  _calendarLabel({String text, Color color, bool ringShape = false}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            width: 20,
+          ),
+          ringShape
+              ? Ring(
+                  color: color,
+                )
+              : Circle(
+                  color: color,
+                ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            text,
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -32,14 +60,56 @@ class CalendarScreen extends StatelessWidget {
             Container(
               child: Column(
                 children: <Widget>[
-                  Text('goal completed'),
-                  Text('current day'),
-                  Text('selected day'),
+                  Column(
+                    children: <Widget>[
+                      _calendarLabel(
+                          text: 'goal completed',
+                          color: PrimaryColor,
+                          ringShape: true),
+                      _calendarLabel(text: 'current Day', color: DarkGreyColor),
+                      _calendarLabel(
+                          text: 'selected Day', color: SecondaryColor),
+                    ],
+                  ),
                 ],
               ),
+            ),
+            SizedBox(
+              height: 20,
             ),
             AdMobUtils.admobBanner(size: "b")
           ],
         )));
+  }
+}
+
+class Circle extends StatelessWidget {
+  final Color color;
+  Circle({this.color});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+      margin: EdgeInsets.symmetric(horizontal: 1.0),
+      height: 20.0,
+      width: 20.0,
+    );
+  }
+}
+
+class Ring extends StatelessWidget {
+  final Color color;
+  Ring({this.color});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          border: Border.all(color: color, width: 3)),
+      margin: EdgeInsets.symmetric(horizontal: 1.0),
+      height: 20.0,
+      width: 20.0,
+    );
   }
 }
