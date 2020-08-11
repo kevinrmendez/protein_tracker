9df326245df3ab0fb6aeab9d6f4c0c53fb10e1db
Kevin Mendez <kevinrmendez@gmail.com>
2020-08-11 16:51:19 +0300

    create csv and send email from settings

--- lib/ui/settingsScreen.dart
+++ lib/ui/settingsScreen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:protein_tracker/bloc/DailyProteinService.dart';
 import 'package:protein_tracker/bloc/SettingsService.dart';
+import 'package:protein_tracker/model/dailyProtein.dart';
 import 'package:protein_tracker/ui/aboutScreen.dart';
 import 'package:protein_tracker/utils/localization_utils.dart';
 import 'package:protein_tracker/utils/widgetUtils.dart';
 import 'package:scidart/numdart.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:csv/csv.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class SettingsScreen extends StatefulWidget {
   SettingsScreen({Key key}) : super(key: key);
@@ -68,8 +74,62 @@ class _SettingsScreenState extends State<SettingsScreen> {
               ],
             ),
           ),
+          ListTile(
+            title: Text(
+              "export data as csv",
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.import_export),
+              onPressed: () async {
+                print('exporting data');
+                _exportData();
+              },
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  void _exportData() async {
+    Directory dir = await getExternalStorageDirectory();
+    String path = dir.absolute.path;
+    print(path);
+    File file = File('${path}/protein.csv');
+    List<DailyProtein> dailyProtein = dailyProteinServices.currentList;
+    List<List<dynamic>> rows = List<List<dynamic>>();
+    for (int i = 0; i < dailyProtein.length; i++) {
+      if (i == 0) {
+        List<dynamic> header = List();
+        header.add("");
+        header.add("date");
+        header.add("goal");
+        header.add("daily protein");
+        header.add("goal achieved");
+        rows.add(header);
+      }
+      List<dynamic> row = List();
+      row.add(dailyProtein[i].id);
+      row.add(dailyProtein[i].date);
+      row.add(dailyProtein[i].goal);
+      row.add(dailyProtein[i].totalProtein);
+      row.add(dailyProtein[i].isGoalAchieved == 1 ? "yes" : "no");
+      rows.add(row);
+    }
+    String csv = const ListToCsvConverter().convert(rows);
+
+    file.writeAsString(csv);
+    String filePath = '$path/protein.csv';
+
+    filePath = file.path;
+
+    final Email email = Email(
+        body: translatedText("email_body", context),
+        subject: translatedText("email_subject", context),
+        // recipients: ['example@example.com'],
+        attachmentPaths: [filePath]);
+    print("FILEPATHS: ${filePath.toString()}");
+    await FlutterEmailSender.send(email);
+    print('file email sended');
   }
 }
